5cec960121f6a9a1f5b44bd572aa3e6ad2117e42
GeunseokJang <geunseok95@gmail.com>
2020-10-20 04:34:12 +0900

    ui_change1

--- app/src/main/java/com/professionalandroid/apps/capston_meeting/RequestPage/RequestPopUpWindow.kt
+++ app/src/main/java/com/professionalandroid/apps/capston_meeting/RequestPage/RequestPopUpWindow.kt
@@ -2,6 +2,8 @@ package com.professionalandroid.apps.capston_meeting.requestPage
 
 import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.view.Window
 import android.widget.Button
 import android.widget.TextView
@@ -14,10 +16,10 @@ class RequestPopUpWindow(context : Context, mlistener: MyDialogOKClickedListener
     private lateinit var btnCancel : Button
     private val listener = mlistener
     fun start(message : String, success: Int) {
-        dlg.requestWindowFeature(Window.FEATURE_NO_TITLE)   //타이틀바 제거
-        dlg.setContentView(R.layout.popup_window_request)     //다이얼로그에 사용할 xml 파일을 불러옴
-        dlg.setCancelable(false)    //다이얼로그의 바깥 화면을 눌렀을 때 다이얼로그가 닫히지 않도록 함
-
+        dlg.requestWindowFeature(Window.FEATURE_NO_TITLE)   // 타이틀바 제거
+        dlg.setContentView(R.layout.popup_window_request)     // 다이얼로그에 사용할 xml 파일을 불러옴
+        dlg.setCancelable(false)    // 다이얼로그의 바깥 화면을 눌렀을 때 다이얼로그가 닫히지 않도록 함
+        dlg.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT)) // 다이얼로그 배경 투명
         lblDesc = dlg.findViewById(R.id.message)
         lblDesc.text = message
 
